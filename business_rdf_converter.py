d159d731b69cebc33a4e00a5629671c2999cdb54
Julian Hotter <julian.hotter@student.uibk.ac.at>
2023-12-13 16:45:13 +0100

    attempt to add classes and starting a drawio

--- business_rdf_converter.py
+++ business_rdf_converter.py
@@ -1,5 +1,5 @@
 import json
-from rdflib import Graph, Namespace, Literal, URIRef
+from rdflib import Graph, Namespace, Literal, URIRef, RDF
 
 # Load JSON data
 with open('./yelp_dataset/yelp_academic_dataset_business.json', 'r') as file:
@@ -11,22 +11,22 @@ g = Graph()
 # Define schema.org namespace
 schema = Namespace("http://schema.org/")
 
-# Define mappings between JSON properties and schema.org properties
+# Define mappings between JSON properties and schema.org properties and classes
 property_mappings = {
-    "business_id": schema.identifier,
-    "name": schema.name,
-    "address": schema.address,
-    "city": schema.addressLocality,
-    "state": schema.addressRegion,
-    "postal_code": schema.postalCode,
-    "latitude": schema.latitude,
-    "longitude": schema.longitude,
-    "stars": schema.ratingValue,
-    "review_count": schema.reviewCount,
-    "is_open": schema.openingHoursSpecification,
-    "attributes": schema.additionalProperty,
-    "categories": schema.category,
-    "hours": schema.hoursAvailable,
+    "business_id": (schema.identifier, None),
+    "name": (schema.name, schema.LocalBusiness),
+    "address": (schema.address, schema.LocalBusiness),
+    "city": (schema.addressLocality, schema.LocalBusiness),
+    "state": (schema.addressRegion, schema.LocalBusiness),
+    "postal_code": (schema.postalCode, schema.LocalBusiness),
+    "latitude": (schema.latitude, schema.GeoCoordinates),  # Class for latitude
+    "longitude": (schema.longitude, schema.GeoCoordinates),  # Class for longitude
+    "stars": (schema.ratingValue, schema.Rating),  # Map "stars" to "Rating" type
+    "review_count": (schema.reviewCount, schema.Rating),
+    "is_open": (schema.openingHoursSpecification, schema.LocalBusiness),
+    "attributes": (schema.additionalProperty, schema.LocalBusiness),
+    "categories": (schema.category, schema.LocalBusiness),
+    "hours": (schema.hoursAvailable, schema.LocalBusiness),
 }
 
 # Iterate through each business in the JSON data
@@ -34,8 +34,11 @@ for business in data:
     # Create a unique URI for the business
     business_uri = URIRef(f"http://example.com/business/{business['business_id']}")
 
-    # Add triples for each property
-    for json_property, schema_property in property_mappings.items():
+    # Add class for LocalBusiness
+    g.add((business_uri, RDF.type, schema.LocalBusiness))
+
+    # Add triples for each property and its class
+    for json_property, (schema_property, property_class) in property_mappings.items():
         if json_property in business and business[json_property] is not None:
             # Special handling for attributes and hours
             if json_property == "attributes":
@@ -45,7 +48,9 @@ for business in data:
                 for day, hours in business[json_property].items():
                     g.add((business_uri, schema_property, Literal(f"{day} {hours}", lang="en")))
             else:
+                if property_class:
+                    # Add a triple indicating the type of the property value
+                    g.add((business_uri, schema_property, RDF.type, property_class))
                 g.add((business_uri, schema_property, Literal(business[json_property], lang="en")))
-
 # Serialize the RDF graph to a file
 g.serialize(destination='output.rdf', format='xml')
