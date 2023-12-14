f67399891bf4ebdb7444a327bd7e965f6e2ca37f
csaw5866 <joey.hieronimy@student.uibk.ac.at>
2023-12-14 10:25:10 +0100

    added functionality for provenance info

--- business_rdf_converter.py
+++ business_rdf_converter.py
@@ -1,17 +1,15 @@
 import json
-from rdflib import Graph, Namespace, Literal, URIRef, RDF
+from rdflib import Graph, Namespace, Literal, URIRef, RDF, RDFS, XSD
+from datetime import datetime
 
-# Load JSON data
 with open('./yelp_dataset/yelp_academic_dataset_business.json', 'r') as file:
     data = json.load(file)
 
-# Create an RDF graph
 g = Graph()
 
-# Define schema.org namespace
 schema = Namespace("http://schema.org/")
 
-# Define mappings between JSON properties and schema.org properties and classes
+# define mappings
 property_mappings = {
     "business_id": (schema.identifier, None),
     "name": (schema.name, schema.LocalBusiness),
@@ -29,18 +27,18 @@ property_mappings = {
     "hours": (schema.hoursAvailable, schema.LocalBusiness),
 }
 
-# Iterate through each business in the JSON data
+# iterate through each business
 for business in data:
-    # Create a unique URI for the business
+    # create a unique URI
     business_uri = URIRef(f"http://example.com/business/{business['business_id']}")
 
-    # Add class for LocalBusiness
+    # add class for LocalBusiness
     g.add((business_uri, RDF.type, schema.LocalBusiness))
 
-    # Add triples for each property and its class
+    # add triples
     for json_property, (schema_property, property_class) in property_mappings.items():
         if json_property in business and business[json_property] is not None:
-            # Special handling for attributes and hours
+            # special handling for attributes and hours
             if json_property == "attributes":
                 for attr_key, attr_value in business[json_property].items():
                     g.add((business_uri, schema_property, Literal(attr_value, lang="en")))
@@ -50,10 +48,17 @@ for business in data:
             elif json_property in ["latitude", "longitude"]:
                 g.add((business_uri, schema_property, Literal(business[json_property])))
             else:
-                # Check if property_class is not None before adding the triple
+                # check if property_class is not None
                 if property_class:
                     g.add((business_uri, RDF.type, property_class))
                 g.add((business_uri, schema_property, Literal(business[json_property], lang="en")))
 
-# Serialize the RDF graph to a file
-g.serialize(destination='./yelp_dataset/business_withClasses.rdf', format='xml')
+# provenance information
+provenance_uri = URIRef("http://example.com/provenance")
+g.add((provenance_uri, RDF.type, schema.Provenance))
+g.add((provenance_uri, schema.creationDate, Literal(datetime.now().isoformat(), datatype=XSD.dateTime)))
+g.add((provenance_uri, schema.dataSource, Literal("https://www.yelp.com/dataset", lang="en")))
+g.add((provenance_uri, schema.numberOfTriples, Literal(len(g), datatype=XSD.integer)))
+
+# export to a file
+g.serialize(destination='./yelp_dataset/business_withClasses_and_provenance.rdf', format='xml')
