5c0f64d3346654bcc353c9d788d4bc3a38ba4607
csaw5866 <joey.hieronimy@student.uibk.ac.at>
2024-01-10 20:09:58 +0100

    update some scripts

--- hotels_dataset/hotel_duplicates_filter.py
+++ hotels_dataset/hotel_duplicates_filter.py
@@ -2,21 +2,19 @@ import csv
 from collections import defaultdict
 
 def filter_duplicates(csv_file_path, output_csv_path):
-    # Create a dictionary to store unique hotels based on address and city
+    
     unique_hotels = defaultdict(list)
 
-    # Open the CSV file and iterate through each row
+    #read
     with open(csv_file_path, 'r', encoding='utf-8') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         for row in csv_reader:
-            # Create a unique identifier for each hotel based on address and city
+            # filter out when adress and city overlap (not address only since many streets are named the same)
             hotel_identifier = (row['address'], row['city'])
 
-            # If the hotel is not in the dictionary, add it
             if hotel_identifier not in unique_hotels:
                 unique_hotels[hotel_identifier].append(row)
-
-    # Write the filtered hotels to a new CSV file
+    # write output
     with open(output_csv_path, 'w', encoding='utf-8', newline='') as output_csv:
         fieldnames = ['address', 'categories', 'city', 'country', 'latitude', 'longitude', 'name',
                       'postalCode', 'province']
@@ -24,15 +22,12 @@ def filter_duplicates(csv_file_path, output_csv_path):
         csv_writer.writeheader()
         for hotels in unique_hotels.values():
             for hotel in hotels:
-                # Write only the non-duplicate hotel columns to the output CSV
                 csv_writer.writerow({key: value for key, value in hotel.items() if key in fieldnames})
 
 if __name__ == "__main__":
-    # Replace 'filtered_data_without_reviews.csv' with the path to the previous filtered CSV file
-    csv_file_path = 'hotels_data_noReviews.csv'
 
-    # Replace 'filtered_data_no_duplicates.csv' with the desired output CSV file path
-    output_csv_path = 'hotels_data_filtered.csv'
+    csv_file_path = './hotels_dataset/hotels_data_noReviews.csv'
 
-    # Filter duplicates based on address and city and write to CSV
+    output_csv_path = './hotels_dataset/hotels_data_filtered.csv'
+
     filter_duplicates(csv_file_path, output_csv_path)
