793497b86e304442d39245b3280b5d4ef113a8f0
csaw5866 <joey.hieronimy@student.uibk.ac.at>
2024-01-11 09:09:16 +0100

    fixes to RDF converter

--- hotels_dataset/hotel_rdfconverter.py
+++ hotels_dataset/hotel_rdfconverter.py
@@ -1,11 +1,10 @@
 import csv
 from rdflib import Graph, Namespace, Literal, URIRef
-from rdflib.namespace import RDF, RDFS
+from rdflib.namespace import RDF, XSD
 
 schema = Namespace("http://schema.org/")
 
-def convert_csv_to_rdf(csv_file_path):
-    
+def convert_csv_to_rdf(csv_file_path, output_file_path):
     graph = Graph()
 
     with open(csv_file_path, 'r', encoding='utf-8') as csv_file:
@@ -13,30 +12,31 @@ def convert_csv_to_rdf(csv_file_path):
         for row in csv_reader:
             # URI
             name_for_uri = row['name'].replace(' ', '_')
-            instance_uri = URIRef(f"http://example.com/hotel/{name_for_uri}")
+            city_for_uri = row['city'].replace(' ', '_')
+            
+            # Adding city to URI to distinguish hotels with the same name
+            instance_uri = URIRef(f"http://example.com/hotel/{name_for_uri}_{city_for_uri}")
 
             graph.add((instance_uri, RDF.type, schema.Hotel))
 
-            # Properties
-            graph.add((instance_uri, schema.name, Literal(row['name'])))
-            graph.add((instance_uri, schema.address, Literal(row['address'])))
-            graph.add((instance_uri, schema.city, Literal(row['city'])))
-            graph.add((instance_uri, schema.country, Literal(row['country'])))
-            graph.add((instance_uri, schema.latitude, Literal(row['latitude'])))
-            graph.add((instance_uri, schema.longitude, Literal(row['longitude'])))
-            graph.add((instance_uri, schema.postalCode, Literal(row['postalCode'])))
-            graph.add((instance_uri, schema.province, Literal(row['province'])))
+            graph.add((instance_uri, schema.name, Literal(row['name'], lang='en')))
+            graph.add((instance_uri, schema.address, Literal(row['address'], lang='en')))
+            graph.add((instance_uri, schema.city, Literal(row['city'], lang='en')))
+            graph.add((instance_uri, schema.country, Literal(row['country'], lang='en')))
 
-    return graph
+            # Check if latitude and longitude are not empty before converting to float
+            if row['latitude']:
+                graph.add((instance_uri, schema.latitude, Literal(float(row['latitude']), datatype=XSD.double)))
+            if row['longitude']:
+                graph.add((instance_uri, schema.longitude, Literal(float(row['longitude']), datatype=XSD.double)))
 
-def save_rdf_to_file(rdf_graph, output_file_path):
-    rdf_graph.serialize(destination=output_file_path, format='turtle')
+            graph.add((instance_uri, schema.postalCode, Literal(row['postalCode'], lang='en')))
+            graph.add((instance_uri, schema.province, Literal(row['province'], lang='en')))
+
+    graph.serialize(destination=output_file_path, format='xml')
 
 if __name__ == "__main__":
     csv_file_path = './hotels_dataset/hotels_data_filtered.csv'
 
     output_file_path = './hotels_dataset/hotels.rdf'
-
-    rdf_graph = convert_csv_to_rdf(csv_file_path)
-
-    save_rdf_to_file(rdf_graph, output_file_path)
+    convert_csv_to_rdf(csv_file_path, output_file_path)
